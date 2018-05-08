462ee41734a096405d8890a7720c568a4fd5166c
John Coleman <johncoleman_2@hotmail.com>
2018-05-08 02:08:03 -0500

    more help

--- models/locationId.js
+++ models/locationId.js
@@ -28,6 +28,127 @@ module.exports = function (sequelize, DataTypes) {
                 len: [1, 200]
             }
         },
+        location_city: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 200]
+            }
+        },
+        location_state: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 2]
+            }
+        },
+        location_zip: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1, 5]
+            }
+        },
+        open_sunday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        open_monday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        open_tuesday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        open_wednesday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        open_thursday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        open_friday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        open_saturday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        close_sunday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        close_monday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        close_tuesday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        close_wednesday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        close_thursday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        close_friday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        close_saturday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        kitchen_close_sunday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        kitchen_close_monday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        kitchen_close_tuesday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        kitchen_close_wednesday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        kitchen_close_thursday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        kitchen_close_friday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        kitchen_close_saturday: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        happy_hour_start: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        happy_hour_stop: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        currently_open: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        },
+        kitchen_currently_open: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        },
         place_id: {
             type: DataTypes.STRING,
             primaryKey: true,
@@ -37,18 +158,14 @@ module.exports = function (sequelize, DataTypes) {
             }
         },
         place_long: {
-            type: DataTypes.BIGINT,
+            type: DataTypes.DECIMAL(3,15),
             allowNull: false,
-            validate: {
-                len: [1, 64]
-            }
+            validate: { min: -90.000000000000, max: 90.999999999999 }
         },
         place_lat: {
-            type: DataTypes.BIGINT,
+            type: DataTypes.DECIMAL(3,15),
             allowNull: false,
-            validate: {
-                len: [1, 64]
-            }
+            validate: { min: -180.000000000000, max: 180.999999999999 }
         },
         family_friendly: {
             type: DataTypes.BOOLEAN,
